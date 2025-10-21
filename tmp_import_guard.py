28b2b7c1314a4590dc1e8d1722a7f7fb258342f5
Olivia G. d'Aliberti <od2961@rinse.cs.princeton.edu>
2025-10-21 11:47:22 -0400

    changes

--- tmp_import_guard.py
+++
@@ -1,5 +0,0 @@
-
-try:
-    from datasets import load_dataset  # type: ignore
-except ImportError:
-    load_dataset = None
--- tmp_test.py
+++
@@ -1,5 +0,0 @@
-
-# pylint: disable=too-many-positional-arguments
-
-def foo(a,b,c,d,e,f,g,h):
-    return a
